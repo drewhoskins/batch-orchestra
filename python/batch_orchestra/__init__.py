87e33a37045dcfdcabcbf6b358059fd49b0965fa
Drew Hoskins <drew.hoskins@temporal.io>
2025-03-16 13:44:29 -0700

    Version++

---
+++ python/batch_orchestra/__init__.py
@@ -0,0 +1,13 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+    try:
+        __version__ = version("batch-orchestra")
+    except PackageNotFoundError:
+        __version__ = "unknown"
+except ImportError:
+    # support python < 3.8; this code came from Claude
+    try:
+        import pkg_resources
+        __version__ = pkg_resources.get_distribution("batch-orchestra").version
+    except:
+        __version__ = "unknown"
