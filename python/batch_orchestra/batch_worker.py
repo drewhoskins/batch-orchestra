63cd910aeb556d942732e59daf87fb9ec36aa93b
Drew Hoskins <drew.hoskins@temporal.io>
2025-03-23 20:55:34 -0700

    Re-add Abstract base class declaration for clarity

--- python/batch_orchestra/batch_worker.py
+++ python/batch_orchestra/batch_worker.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from abc import ABC
 from typing import Optional
 
 import temporalio.client
@@ -55,7 +56,7 @@ Used by your @page_processor and @batch_tracker.
 """
 
 
-class BatchWorkerContext:
+class BatchWorkerContext(ABC):  # noqa: B024 this class shouldn't be instantiated
     def __init__(self, activity_info: activity.Info):
         self._activity_info = activity_info
         self._parent_workflow: Optional[WorkflowHandle] = None
