185bd7af67198da3836acbc1ea8da9fd76979c6a
Drew Hoskins <drew.hoskins@temporal.io>
2025-03-23 20:50:12 -0700

    Fix ruff

--- python/batch_orchestra/batch_orchestrator.py
+++ python/batch_orchestra/batch_orchestrator.py
@@ -9,11 +9,10 @@ import inflect
 from batch_orchestrator_io import BatchOrchestratorInput, BatchOrchestratorProgress
 from batch_processor import BatchPage, PageProcessor, get_page_processor, process_page
 from batch_tracker import track_batch_progress
+from internal.state import ContinueAsNewState, EnqueuedPage, PageTrackerData
 from temporalio import workflow
 from temporalio.common import RetryPolicy
 from temporalio.exceptions import ActivityError, ApplicationError, CancelledError
-
-from internal.state import ContinueAsNewState, EnqueuedPage, PageTrackerData
 
 #
 # batch_orchestrator library
